2839bcae6b9dade3f3642ac9692b0d4663610b63
George Hart <georgehart@GeorgeMBP.local>
2020-08-08 14:25:55 -0700

    Installed and added Apollo support for GraphQL.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient, { gql } from "apollo-boost";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+const cache = new InMemoryCache();
+
+const uri = "http://localhost:8000/graphql/";
+
+export const client = new ApolloClient({
+  cache,
+  uri,
+  fetchOptions: {
+    credentials: "include",
+  },
+  request: (operation) => {
+    const token = localStorage.getItem("authToken") || "";
+    operation.setContext({
+      headers: {
+        Authorization: `JWT ${token}`,
+      },
+    });
+  },
+  clientState: {
+    defaults: {
+      isLoggedIn: !!localStorage.getItem("authToken"),
+    },
+  },
+});
+
+export const IS_LOGGED_IN = gql`
+  {
+    isLoggedIn @client
+  }
+`;
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>,
+
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
