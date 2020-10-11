65a0804946e6e001585f4d5d63345c53666d7acc
George Hart <hamiltonhart@protonmail.com>
2020-10-11 13:06:21 -0700

    Added the ability to search items in a collection.

--- frontend/src/components/CollectionItems/CollectionItemsList.js
+++ frontend/src/components/CollectionItems/CollectionItemsList.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { CollectionItem } from "./CollectionItem";
 
+import { makeStyles, TextField } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  searchField: {
+    marginBottom: theme.spacing(2),
+  },
+}));
+
 export const CollectionItemsList = ({ items, collectionId }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [listItems, setListItems] = useState(items);
+
+  const handleSearch = (e) => {
+    const searchArray = new Array();
+    const tempSearchTerm = e.target.value;
+    items.map((item) => {
+      item.movie.title.toLowerCase().includes(tempSearchTerm.toLowerCase()) &&
+        searchArray.push(item);
+    });
+    setSearchTerm(tempSearchTerm);
+    setListItems(searchArray);
+  };
+
+  const classes = useStyles();
+
   return (
     <div>
-      {items.map((item) => (
+      <TextField
+        className={classes.searchField}
+        fullWidth
+        variant="outlined"
+        value={searchTerm}
+        onChange={(e) => handleSearch(e)}
+        placeholder="Search Collection"
+      />
+      {listItems.map((item) => (
         <CollectionItem key={item.id} item={item} collectionId={collectionId} />
       ))}
     </div>
