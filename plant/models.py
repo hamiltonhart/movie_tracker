b8287a08b381ca0263b527e52f01d29a8fe863bb
George Hart <hamiltonhart@protonmail.com>
2021-06-25 11:30:32 -0700

    Updated Plant model to include watering instructions and comments.

--- plant/models.py
+++ plant/models.py
@@ -5,6 +5,8 @@ class Plant(models.Model):
     name = models.CharField(max_length=100)
     sci_name = models.CharField(max_length=100, default="")
     type = models.CharField(max_length=100)
+    watering_instructions = models.TextField(default="")
+    comments = models.TextField(default="")
 
     def __str__(self):
         return self.name
